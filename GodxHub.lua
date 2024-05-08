072bce6fe0bfd2f90cd1b12d3ce0240969f84c76
obfchx <162278297+obfchx@users.noreply.github.com>
2024-05-08 21:49:43 +0700

    GodxHub.lua

--- GodxHub.lua
+++ GodxHub.lua
@@ -23,11 +23,9 @@ local ScreenGui1 = Instance.new("ScreenGui")
 local ImageButton1 = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
 local UIStroke = Instance.new("UIStroke")
-
 ScreenGui1.Name = "ImageButton"
 ScreenGui1.Parent = game.CoreGui
 ScreenGui1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
 ImageButton1.Parent = ScreenGui1
 ImageButton1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 ImageButton1.BorderSizePixel = 0
@@ -35,10 +33,6 @@ ImageButton1.Position = UDim2.new(0, 175, 0, 40)
 ImageButton1.Size = UDim2.new(0, 480, 0, 250)
 ImageButton1.Draggable = false
 ImageButton1.Image = "http://www.roblox.com/asset/?id=17393812853"
-ImageButton1.MouseButton1Down:connect(function()
-  game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
-  game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
-end)
 UICorner.Parent = ImageButton1
 UIStroke.Thickness = 0.5
 UIStroke.Parent = ImageButton1
